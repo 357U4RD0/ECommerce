84cb706b3e986e1f2ba8e0173f80b31b468c5c40
Estuardo Castro <cas23890@uvg.edu.gt>
2025-05-22 20:40:29 -0600

    Cosas varias

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,26 @@
-import React, {useState} from "react";
-
+import React, {useState} from 'react'
+//import { useNavigate } from 'react-router-dom';
 const ProductCard = ({ product }) => {
     const [count, setCount] = useState(0);
+    //const navigate = useNavigate();
+
+    const DirImagen = () => {
+     // navigate()
+    }
 
     const ClickPrueba = () => {
         setCount(cuenta => cuenta + 1);
         console.log(`${product.name} ${count + 1}`);
     }
   
-  
     return (
     <div className="card">
-      <img src={product.img} alt={product.name} className="card-img" />
+      <img onDoubleClick={DirImagen} src={product.img} alt={product.name} className="card-img" />
       <h3>{product.name}</h3>
       <p>${product.price.toFixed(2)}</p>
       <button onClick = {ClickPrueba}>Añadir</button>
-    </div>
+      </div>
   );
-};
+}
 
 export default ProductCard;
