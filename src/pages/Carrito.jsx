0dcc294ad4e49ccffa6d7070c589a170ca29b4b4
Estuardo Castro <cas23890@uvg.edu.gt>
2025-05-31 20:16:48 -0600

    Arreglar inconvenientes con carrito

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -19,23 +19,43 @@ const Carrito = () => {
 
   return (
     <div className="carrito-container">
-      <div className="carrito-header"><div>PRODUCTO</div><div>PRECIO</div><div>CANTIDAD</div><div>SUBTOTAL</div></div>
-      {carrito.map(p => (
-        <div className="carrito-item" key={p.id}>
-          <div className="producto-info"><img src={p.img} alt={p.name} className="producto-imagen" /><span>{p.name}</span></div>
-          <div>${p.price.toFixed(2)}</div>
-          <select value={cantidades[p.id]} onChange={e => cambiarCantidad(p.id, +e.target.value)}>
-            {[...Array(9)].map((_, i) => <option key={i + 1} value={i + 1}>{i + 1}</option>)}
-          </select>
-          <div>${(p.price * (cantidades[p.id] || 1)).toFixed(2)}</div>
+      <div className="carrito-header">
+        <div>PRODUCTO</div>
+        <div>PRECIO</div>
+        <div>CANTIDAD</div>
+        <div>SUBTOTAL</div>
+      </div>
+
+      <div className="carrito-scroll">
+        {carrito.map(p => (
+          <div className="carrito-item" key={p.id}>
+            <div className="producto-info">
+              <img src={p.img} alt={p.name} className="producto-imagen" />
+              <span>{p.name}</span>
+            </div>
+            <div>${p.price.toFixed(2)}</div>
+            <select value={cantidades[p.id]} onChange={e => cambiarCantidad(p.id, +e.target.value)}>
+              {[...Array(9)].map((_, i) => (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}
+                </option>
+              ))}
+            </select>
+            <div>${(p.price * (cantidades[p.id] || 1)).toFixed(2)}</div>
+          </div>
+        ))}
+      </div>
+
+      <div className="carrito-footer">
+        <div className="carrito-total">
+          <span>TOTAL</span>
+          <span>{mostrarTotal}</span>
         </div>
-      ))}
-      
-      <div className="carrito-total"><span>TOTAL</span><span>{mostrarTotal}</span></div>
-      <div className="carrito-actions">
-        <button className="btn-vaciar" onClick={vaciar}>🗑️ Vaciar</button>
-        <button className="btn-volver" onClick={() => navigate('/')}>↶ Volver</button>
-        <button className="btn-pagar">$ Pagar</button>
+        <div className="carrito-actions">
+          <button className="btn-vaciar" onClick={vaciar}>🗑️ Vaciar</button>
+          <button className="btn-volver" onClick={() => navigate('/')}>↶ Volver</button>
+          <button className="btn-pagar">$ Pagar</button>
+        </div>
       </div>
     </div>
   );
